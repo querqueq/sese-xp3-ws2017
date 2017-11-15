5ea1230844de5a62f5750c6dc87a666bd93d6322
Alwaysgone <ffxiizocker@hotmail.com>
2017-11-15 20:03:46 +0100

    added mal sex test #7

---
+++ src/test/java/at/ac/tuwien/student/sese2017/xp/hotelmanagement/SexTest.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.student.sese2017.xp.hotelmanagement;
+
+import static org.junit.Assert.assertEquals;
+import at.ac.tuwien.student.sese2017.xp.hotelmanagement.domain.data.Sex;
+import org.junit.Test;
+
+public class SexTest {
+  
+  @Test
+  public void testMaleSex() {
+    assertEquals(Sex.MALE.getDisplay(), "Männlich");
+  }
+}

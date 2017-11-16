39e9793e14ece2963d25dc2b688ffd8d783642b7
L.Kerck <e9871403@student.tuwien.ac.at>
2017-11-16 22:08:02 +0100

    Fixed style errors

--- src/main/java/at/ac/tuwien/student/sese2017/xp/hotelmanagement/config/AppProperties.java
+++ src/main/java/at/ac/tuwien/student/sese2017/xp/hotelmanagement/config/AppProperties.java
@@ -1,16 +1,20 @@
 package at.ac.tuwien.student.sese2017.xp.hotelmanagement.config;
 
-
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 /**
- * Properties for HotelManagement application
+ * Properties for HotelManagement application.
  */
-@ConfigurationProperties(prefix="appconfig")
+@ConfigurationProperties(prefix = "appconfig")
 @Service
 @Data
 public class AppProperties {
-  private boolean injecttestdata;
+
+  /**
+   * Inject testdata into the database.
+   * Defaults to false
+   */
+  private boolean injectTestdata;
 }

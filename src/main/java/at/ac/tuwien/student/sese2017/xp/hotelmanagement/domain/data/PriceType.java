6f34de6b3f6ce03b970da89f57b07889754b6bbc
Andreas Kraschitzer <6935251+a-kraschitzer@users.noreply.github.com>
2017-11-16 11:35:07 +0100

    translate frontEnd to german

--- src/main/java/at/ac/tuwien/student/sese2017/xp/hotelmanagement/domain/data/PriceType.java
+++ src/main/java/at/ac/tuwien/student/sese2017/xp/hotelmanagement/domain/data/PriceType.java
@@ -1,10 +1,24 @@
 package at.ac.tuwien.student.sese2017.xp.hotelmanagement.domain.data;
 
 public enum PriceType {
-  SINGLE,
-  DOUBLE,
-  TRIPLE,
-  SINGLE_WITH_CHILD,
-  SINGLE_WITH_TWO_CHILDREN,
-  DOUBLE_WITH_CHILD
+  SINGLE("Einzel"),
+  DOUBLE("Doppel"),
+  TRIPLE("Tripel"),
+  SINGLE_WITH_CHILD("Einzel mit Kind"),
+  SINGLE_WITH_TWO_CHILDREN("Einzel mit zwei Kindern"),
+  DOUBLE_WITH_CHILD("Doppel mit Kind");
+
+  private final String text;
+
+  PriceType(final String text) {
+    this.text = text;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Enum#toString()
+   */
+  @Override
+  public String toString() {
+    return text;
+  }
 }

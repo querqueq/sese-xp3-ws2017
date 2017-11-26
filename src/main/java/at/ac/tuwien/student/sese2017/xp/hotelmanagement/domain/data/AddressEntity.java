2ae7dbe9e0b392b92dc5e0bc62df252373339962
L.Kerck <e9871403@student.tuwien.ac.at>
2017-11-26 23:05:49 +0100

    WIP Added entity tests

--- src/main/java/at/ac/tuwien/student/sese2017/xp/hotelmanagement/domain/data/AddressEntity.java
+++ src/main/java/at/ac/tuwien/student/sese2017/xp/hotelmanagement/domain/data/AddressEntity.java
@@ -1,9 +1,17 @@
 package at.ac.tuwien.student.sese2017.xp.hotelmanagement.domain.data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class AddressEntity {
+  @Id
+  @GeneratedValue
+  private long addressId;
+
   private String name;
   private String streetAddress1;
   private String streetAddress2;

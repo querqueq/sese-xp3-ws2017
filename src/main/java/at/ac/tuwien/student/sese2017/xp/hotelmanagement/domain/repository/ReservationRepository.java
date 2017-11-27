a9a65ff983bd4e31a62822439b9f65b6eb72fca2
L.Kerck <e9871403@student.tuwien.ac.at>
2017-11-27 16:56:08 +0100

    WIP Fixed wrong Generic Type order

--- src/main/java/at/ac/tuwien/student/sese2017/xp/hotelmanagement/domain/repository/ReservationRepository.java
+++ src/main/java/at/ac/tuwien/student/sese2017/xp/hotelmanagement/domain/repository/ReservationRepository.java
@@ -3,6 +3,6 @@ package at.ac.tuwien.student.sese2017.xp.hotelmanagement.domain.repository;
 import org.springframework.data.repository.CrudRepository;
 import at.ac.tuwien.student.sese2017.xp.hotelmanagement.domain.data.ReservationEntity;
 
-public interface ReservationRepository extends CrudRepository<Long, ReservationEntity> {
+public interface ReservationRepository extends CrudRepository<ReservationEntity, Long> {
 
 }

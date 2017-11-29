7c2353a653d1570030ecab0465b78f4a39cfc67b
Johannes <e1225449@student.tuwien.ac.at>
2017-11-29 13:29:51 +0100

    Implemented linking receipts for customer :+1:, added city for address

--- src/main/java/at/ac/tuwien/student/sese2017/xp/hotelmanagement/service/ReceiptService.java
+++ src/main/java/at/ac/tuwien/student/sese2017/xp/hotelmanagement/service/ReceiptService.java
@@ -1,7 +1,9 @@
 package at.ac.tuwien.student.sese2017.xp.hotelmanagement.service;
 
 import at.ac.tuwien.student.sese2017.xp.hotelmanagement.domain.data.ReceiptEntity;
+import at.ac.tuwien.student.sese2017.xp.hotelmanagement.domain.repository.CustomerRepository;
 import at.ac.tuwien.student.sese2017.xp.hotelmanagement.domain.repository.ReceiptRepository;
+import java.util.Collections;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,13 +18,20 @@ import org.springframework.stereotype.Service;
 public class ReceiptService {
 
   private final ReceiptRepository receiptRepository;
+  private final CustomerRepository customerRepository;
 
   @Autowired
-  public ReceiptService(ReceiptRepository receiptRepository) {
+  public ReceiptService(ReceiptRepository receiptRepository, CustomerRepository customerRepository) {
     this.receiptRepository = receiptRepository;
+    this.customerRepository = customerRepository;
   }
 
   public List<ReceiptEntity> search(String searchText) {
     return receiptRepository.search(searchText);
+  }
+  
+  public List<ReceiptEntity> getReceiptsForCustomer(Long customerId) {
+    return customerRepository.findById(customerId).map(c -> c.getReceipts())
+        .orElse(Collections.emptyList());
   }
 }
